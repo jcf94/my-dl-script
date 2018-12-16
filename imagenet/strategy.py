cce737a304f0312401ef605e8bee97ed0852a2d7
jcf94 <xff252595680@gmail.com>
2018-12-16 20:16:57 +0800

    Add Distributed Strategy

--- imagenet/strategy.py
+++ imagenet/strategy.py
@@ -156,3 +156,64 @@ class LocalStagingStrategy(object):
                 apply_op = optimizer.apply_gradients(grads_and_varis, global_step)
 
         return [input_staging_op, gradients_put_op, apply_op]
+
+class DistributedPSStrategy(object):
+    def __init__(self, BenchMark):
+        tf.logging.info('Using LocalPSStrategy')
+
+        self._cpu_device = BenchMark.cpu_device
+        self._num_workers = BenchMark._num_workers
+        self._num_gpus = BenchMark._num_gpus
+        self._total_gpus = self._num_workers * self._num_gpus
+        self._param_server_device = BenchMark._param_server_device
+
+        self._global_variable = {}
+        self._local_variable = [
+            [dict() for _ in range(self._num_gpus)] for _ in range(self._num_workers)
+        ]
+        self._local_sizes = [0] * self._num_workers
+
+    def __call__(self, getter, name, *args, **kwargs):
+        name_split = name.split('/', 2)
+        worker_index = int(name_split[1].split('_')[1])
+        gpu_index = int(name_split[1].split('_')[2])
+        name_without_tower = name_split[0] + '/' + name_split[2]
+
+        if (name_without_tower in self._global_variable):
+            global_var = self._global_variable[name_without_tower]
+        else:
+            min_size_device, _ = min(enumerate(self._local_sizes), key=operator.itemgetter(1))
+            with tf.device(self._cpu_device[min_size_device]):
+                global_var = getter(name_without_tower, *args, **kwargs)
+            self._local_sizes[min_size_device] += global_var.get_shape().num_elements()
+            self._global_variable[name_without_tower] = global_var
+
+        self._local_variable[worker_index][gpu_index][name_without_tower] = global_var
+
+        return self._local_variable[worker_index][gpu_index][name_without_tower]
+
+    def get_local_variable(self, worker_index, gpu_index):
+        return [v for k,v in self._local_variable[worker_index][gpu_index].items()]
+    
+    def get_global_variable(self):
+        return [v for k,v in self._global_variable.items()]
+    
+    def compute_gradient_and_apply(self, gradients_list, global_step):
+        optimizer = tf.train.GradientDescentOptimizer(0.001)
+
+        with tf.name_scope('Gradient_Update'):
+            global_varis = self.get_global_variable()
+
+            apply_list = []
+            for g_v in zip(*gradients_list, global_varis):
+                grads = g_v[:self._total_gpus]
+                varis = g_v[self._total_gpus]
+                with tf.device(varis.device):
+                    average_grad = tf.multiply(tf.add_n(grads), 1.0 / self._total_gpus)
+                    apply = optimizer.apply_gradients([(average_grad, varis)])
+                    apply_list.append(apply)
+            with tf.device(global_step.device):
+                apply_list.append(global_step.assign_add(1))
+            apply_op = tf.group(apply_list)
+
+        return [apply_op]
