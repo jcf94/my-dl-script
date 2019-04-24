345a9f81b1000b4e7f6de665e9d52d8d01f2bf1e
jcf_work <jcf@jcf-works-MacBook-Pro.local>
2019-04-24 16:25:07 -0700

    Add transformer record data check

---
+++ transformer/check_record_data.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
+
+def _parse_example(serialized_example):
+    """Return inputs and targets Tensors from a serialized tf.Example."""
+    data_fields = {
+        "inputs": tf.VarLenFeature(tf.int64),
+        "targets": tf.VarLenFeature(tf.int64)
+    }
+    parsed = tf.parse_single_example(serialized_example, data_fields)
+    inputs = tf.sparse_tensor_to_dense(parsed["inputs"])
+    targets = tf.sparse_tensor_to_dense(parsed["targets"])
+    return inputs, targets
+
+filename = ["translate_ende/wmt32k-train-00001-of-00100"]
+dataset = tf.data.TFRecordDataset(filename)
+
+dataset = dataset.map(_parse_example)
+iterator = dataset.make_initializable_iterator()
+
+sess = tf.Session()
+sess.run(iterator.initializer)
+
+for i in range(10):
+    out = sess.run(iterator.get_next())
+    print(out[0].shape, " ", out[1].shape)
