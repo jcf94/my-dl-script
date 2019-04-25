28d0899c2454c65a05d9ec1ec60aa62428d89ca7
jcf_work <jcf@jcf-works-MacBook-Pro.local>
2019-04-24 17:35:36 -0700

    Add start script

---
+++ transformer/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /root/miniconda/bin/activate iris_env_36
+
+DATA_DIR=/tmp/translate_ende
+VOCAB_FILE=${DATA_DIR}/vocab.ende.32768
+
+FLAGS="--data_dir=${DATA_DIR}
+       --vocab_file=${VOCAB_FILE}
+       --param_set=base
+       --batch_size=2048
+       --num_gpus=1"
+
+python transformer_main.py ${FLAGS}
